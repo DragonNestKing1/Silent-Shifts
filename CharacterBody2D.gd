b89a41ebecc63009641e4e73496a30ebaafa79da
DragonNestKing1 <156347198+DragonNestKing1@users.noreply.github.com>
2024-03-13 11:14:46 -0500

    V0.02
    A

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -2,10 +2,22 @@ extends CharacterBody2D
 
 @export var speed = 400
 
-func get_input():
+func movement():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
+	
+func invisibility():
+	var invis_button = Input.is_action_just_pressed("invisibility toggle")
+
+	if invis_button:
+		if get_node("Icon2").modulate.a == 1:
+			get_node("Icon2").modulate.a = 0.5
+			get_node("CollisionShape2D2").
+		elif get_node("Icon2").modulate.a == 0.5:
+			get_node("Icon2").modulate.a = 1
+
 
 func _physics_process(delta):
-	get_input()
+	movement()
 	move_and_slide()
+	invisibility()
