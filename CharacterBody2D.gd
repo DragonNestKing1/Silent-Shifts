83005040b87823afbee4e0d8a8490bb176656693
DragonNestKing1 <156347198+DragonNestKing1@users.noreply.github.com>
2024-03-14 11:06:54 -0500

    Animation and Invisibility V0.03
    The sprite now has a walking animation, and invisibility is now functional!

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -6,16 +6,27 @@ func movement():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 	
+	if velocity.x == 0 and velocity.y == 0:
+		$Icon2.play("idle")
+	else:
+		if velocity.x > 0:
+			$Icon2.play("walk")
+			$Icon2.flip_h = false
+		elif velocity.x < 0:
+			$Icon2.play("walk")
+			$Icon2.flip_h = true
+	
 func invisibility():
 	var invis_button = Input.is_action_just_pressed("invisibility toggle")
 
 	if invis_button:
 		if get_node("Icon2").modulate.a == 1:
 			get_node("Icon2").modulate.a = 0.5
-			get_node("CollisionShape2D2").
+			$CollisionShape2D2.set_deferred("disabled", true)
 		elif get_node("Icon2").modulate.a == 0.5:
 			get_node("Icon2").modulate.a = 1
-
+			$CollisionShape2D2.set_deferred("disabled", false)
+var code_work = true
 
 func _physics_process(delta):
 	movement()
