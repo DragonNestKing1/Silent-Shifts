4ad173e9bbaf1e07487c506e4d9f45ea9f388be8
DragonNestKing1 <156347198+DragonNestKing1@users.noreply.github.com>
2024-03-27 11:05:06 -0500

    Silent Shifts Talking Update 1
    There is now a chat bubble that pops up when an entity is in range and the button "e" is pressed.

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -1,34 +1,54 @@
 extends CharacterBody2D
 
+var code_work = true
+var talking_range = false
 @export var speed = 400
+
+
 
 func movement():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 	
 	if velocity.x == 0 and velocity.y == 0:
-		$Icon2.play("idle")
+		$Character.play("idle")
 	else:
 		if velocity.x > 0:
-			$Icon2.play("walk")
-			$Icon2.flip_h = false
+			$Character.play("walk")
+			$Character.flip_h = false
 		elif velocity.x < 0:
-			$Icon2.play("walk")
-			$Icon2.flip_h = true
+			$Character.play("walk")
+			$Character.flip_h = true
 	
 func invisibility():
 	var invis_button = Input.is_action_just_pressed("invisibility toggle")
 
 	if invis_button:
-		if get_node("Icon2").modulate.a == 1:
-			get_node("Icon2").modulate.a = 0.5
+		if get_node("Character").modulate.a == 1:
+			get_node("Character").modulate.a = 0.5
 			$CollisionShape2D2.set_deferred("disabled", true)
-		elif get_node("Icon2").modulate.a == 0.5:
-			get_node("Icon2").modulate.a = 1
+		elif get_node("Character").modulate.a == 0.5:
+			get_node("Character").modulate.a = 1
 			$CollisionShape2D2.set_deferred("disabled", false)
-var code_work = true
 
-func _physics_process(delta):
+func conversation():
+	# use body_entered and body_exited to make a true/false in movement.
+	# use the if true with is_action_just_pressed "interact".
+	# add short dialogue in game, maybe becomes more bubbly with more interactions.
+	if talking_range and Input.is_action_just_pressed("interact"):
+		get_node("Chatbubble").set_visible(true)
+		
+
+func _physics_process(_delta):
 	movement()
 	move_and_slide()
 	invisibility()
+	conversation()
+
+
+func body_entered(_body):
+	talking_range = true
+	
+
+func body_exited(_body):
+	talking_range = false
