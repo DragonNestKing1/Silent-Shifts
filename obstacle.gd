70e58c272d4bb66fc85e7b5f827226d2d4cb6e5e
DragonNestKing1 <156347198+DragonNestKing1@users.noreply.github.com>
2024-04-10 10:46:25 -0500

    Halfway DONE v0.05
    AAAAAAAAAA

--- obstacle.gd
+++ obstacle.gd
@@ -1,5 +1,12 @@
 extends CharacterBody2D
-# @import chatting_now
+
+var character_body: CharacterBody2D = get_parent().get_node("CharacterBody2D")
 
 func _physics_process(_delta):
 
+	if character_body.chatting_now:
+		# Do something when chatting is happening
+		print("Character is currently chatting!")
+	else:
+		# Do something else
+		print("Character is not chatting.")
